74fe2cc2b095fd7793531a646154c84741670dcf
Arnald Franklin <arnald7@gmail.com>
2022-06-28 13:10:12 -0300

    fix display background image

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -3,6 +3,7 @@ import { useState, FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
 import { useCreateSubscriberMutation } from "../graphql/generated";
+import CodeMockup from '../assets/images/code-mockup.png';
 
 
 export function Subscribe() {
@@ -73,7 +74,7 @@ export function Subscribe() {
 
             </div>
 
-            <img src="/src/assets/code-mockup.png" className="mt-10" alt="" />
+            <img src={CodeMockup} className="mt-10" alt="" />
         </div>
     )
 }
